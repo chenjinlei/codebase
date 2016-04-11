9db81408d58f12fa532a625126be2d3d97c6f14f
chenjinlei <chenjinlei456@sina.com>
2016-04-11 11:24:08 +0800

    save password through SharedPreferences

--- android/BroadcastBestPractice/gen/com/example/broadcastbestpractice/R.java
+++ android/BroadcastBestPractice/gen/com/example/broadcastbestpractice/R.java
@@ -26,10 +26,11 @@ public final class R {
     }
     public static final class id {
         public static final int account=0x7f080001;
-        public static final int action_settings=0x7f080004;
+        public static final int action_settings=0x7f080005;
         public static final int force_offline=0x7f080000;
-        public static final int login=0x7f080003;
+        public static final int login=0x7f080004;
         public static final int password=0x7f080002;
+        public static final int remember_pass=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -47,6 +48,7 @@ public final class R {
         public static final int hint_main=0x7f050007;
         public static final int login=0x7f050006;
         public static final int password=0x7f050005;
+        public static final int remember_password=0x7f050008;
     }
     public static final class style {
         /** 
--- android/BroadcastBestPractice/src/com/example/broadcastbestpractice/LoginActivity.java
+++ android/BroadcastBestPractice/src/com/example/broadcastbestpractice/LoginActivity.java
@@ -2,11 +2,14 @@ package com.example.broadcastbestpractice;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -15,6 +18,10 @@ public class LoginActivity extends Activity implements OnClickListener {
 	private EditText accountEdit;
 	private EditText passwordEdit;
 	private Button login;
+
+	private SharedPreferences pref;
+	private SharedPreferences.Editor editor;
+	private CheckBox rememberPass;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +32,21 @@ public class LoginActivity extends Activity implements OnClickListener {
 		accountEdit = (EditText) findViewById(R.id.account);
 		passwordEdit = (EditText) findViewById(R.id.password);
 		login = (Button) findViewById(R.id.login);
-
 		login.setOnClickListener(this);
+
+		rememberPass = (CheckBox) findViewById(R.id.remember_pass);
+		pref = PreferenceManager.getDefaultSharedPreferences(this);
+		boolean isRemembered = pref.getBoolean("remember_password", false);
+
+		if (isRemembered) {
+			String account = pref.getString("account", "");
+			String password = pref.getString("password", "");
+			accountEdit.setText(account);
+			passwordEdit.setText(password);
+			rememberPass.setChecked(true);
+
+		}
+
 	}
 
 	@Override
@@ -36,6 +56,15 @@ public class LoginActivity extends Activity implements OnClickListener {
 			String account = accountEdit.getText().toString();
 			String password = passwordEdit.getText().toString();
 			if (account.equals("admin") && password.equals("123456")) {
+				editor = pref.edit();
+				if (rememberPass.isChecked()) {
+					editor.putBoolean("remember_password", true);
+					editor.putString("account", account);
+					editor.putString("password", password);
+				} else {
+					editor.clear();
+				}
+				editor.commit();
 				Intent intent = new Intent(this, MainActivity.class);
 				startActivity(intent);
 				finish();
