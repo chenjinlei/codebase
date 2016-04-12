768ec9ee46d0ce28c180842fedb74ba4934f999b
chenjinlei <chenjinlei456@sina.com>
2016-04-12 09:25:11 +0800

    read Contacts data from ContentProvider

---
+++ android/ContactsTest/src/com/example/contactstest/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.contactstest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends Activity {
+
+	private ListView contactsView;
+	ArrayAdapter<String> adapter;
+	List<String> contactsList = new ArrayList<String>();
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		contactsView = (ListView) this.findViewById(R.id.contacts_view);
+		adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, contactsList);
+		contactsView.setAdapter(adapter);
+		
+		readContacts();
+	}
+
+	private void readContacts() {
+		Cursor cursor = null;
+		
+		try {
+			cursor = this.getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+					null, null, null, null);
+			while (cursor.moveToNext()) {
+				String displayName = cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+				String number = cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.NUMBER));
+				contactsList.add(displayName + "\n" + number);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (cursor != null) {
+				cursor.close();
+			}
+		}
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
