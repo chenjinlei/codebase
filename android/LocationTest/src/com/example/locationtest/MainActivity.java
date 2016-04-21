d0adaa2b04dd5b291619570e644d3a157b5394df
chenjinlei <chenjinlei456@sina.com>
2016-04-21 10:40:07 +0800

    display the name of location

--- android/LocationTest/src/com/example/locationtest/MainActivity.java
+++ android/LocationTest/src/com/example/locationtest/MainActivity.java
@@ -2,11 +2,24 @@ package com.example.locationtest;
 
 import java.util.List;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.baidu.mapapi.map.BaiduMap;
+
 import android.app.Activity;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -17,6 +30,10 @@ public class MainActivity extends Activity {
 	private TextView positionTextView;
 	private LocationManager locationManager;
 	private String provider;
+
+	private static final int SHOW_LOCATION = 0;
+	
+	private BaiduMap manager;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -81,12 +98,62 @@ public class MainActivity extends Activity {
 		}
 	};
 
-	private void showLocation(Location location) {
-		String currentPosition = "latitude is " + location.getLatitude() + "\n" + "longitude is "
-				+ location.getLongitude();
-		positionTextView.setText(currentPosition);
+	private void showLocation(final Location location) {
+		// String currentPosition = "latitude is " + location.getLatitude() +
+		// "\n" + "longitude is "
+		// + location.getLongitude();
+		// positionTextView.setText(currentPosition);
+
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				try {
+					StringBuilder url = new StringBuilder();
+					url.append("http://maps.googleapis.com/maps/api/geocode/json?latlng=");
+					url.append(location.getLatitude()).append(",");
+					url.append(location.getLongitude());
+					url.append("&sensor=false");
+
+					HttpClient httpClient = new DefaultHttpClient();
+					HttpGet httpGet = new HttpGet(url.toString());
+					httpGet.addHeader("Accept-Language", "zh-CN");
+					HttpResponse httpResponse = httpClient.execute(httpGet);
+					if (httpResponse.getStatusLine().getStatusCode() == 200) {
+						HttpEntity entity = httpResponse.getEntity();
+						String response = EntityUtils.toString(entity, "utf-8");
+						JSONObject jsonObject = new JSONObject(response);
+						JSONArray resultArray = jsonObject.getJSONArray("results");
+						if (resultArray.length() > 0) {
+							JSONObject subObject = resultArray.getJSONObject(0);
+							String address = subObject.getString("formatted_address");
+							Message message = new Message();
+							message.what = SHOW_LOCATION;
+							message.obj = address;
+							handler.sendMessage(message);
+						}
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
 
 	}
+
+	private Handler handler = new Handler() {
+		public void handleMessage(Message msg) {
+
+			switch (msg.what) {
+			case SHOW_LOCATION:
+				positionTextView.setText(msg.obj.toString());
+				break;
+			default:
+				break;
+			}
+		}
+	};
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
